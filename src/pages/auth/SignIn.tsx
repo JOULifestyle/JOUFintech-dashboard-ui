b891ad61efbd3f69356b66cf4ab0c091de7c6fd7
Israel Olasehinde <olasehindeisrael@gmail.com>
2025-11-20 00:58:24 +0100

    Client side routing

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -4,6 +4,7 @@ import { useAuthStore } from "../../stores/authStore";
 import { signIn } from "../../api/auth";
 
 export default function SignIn() {
+  console.log("SignIn component rendered");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
